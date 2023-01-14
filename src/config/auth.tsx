39fff4de18b084abcee7661590df97884904d38e
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-14 19:54:43 -0300

    chore: configure async storage

---
+++ src/config/auth.tsx
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { API_TOKEN_KEY } from '@env'
+const baseKey = API_TOKEN_KEY
+import jwt_decode from 'jwt-decode'
+
+export const setStorageItem = async (key: string, value: string) => {
+  await AsyncStorage.setItem(`${baseKey}${key}`, value)
+}
+
+export const getStorageItem = async (key: string) => {
+  return await AsyncStorage.getItem(`${baseKey}${key}`)
+}
+
+export const removeStorageItem = async (key: string) => {
+  await AsyncStorage.removeItem(`${baseKey}${key}`)
+}
+
+export const decodeToken = async (key = 'token') => {
+  return jwt_decode(await AsyncStorage.getItem(`${baseKey}${key}`) || '')
+}
