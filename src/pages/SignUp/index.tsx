9e9a5c11673a6ce3de8d0ee0bc9c1d8e1e491b97
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-15 17:40:23 -0300

    feat: ensure that the user can register

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { signUpAction } from '../../store/auth/auth.action'
+import { useDispatch } from 'react-redux'
+import FormSignUp from '../../components/auth/SignUp/index'
+import { useNavigation } from '@react-navigation/native'
+import { Alert } from 'react-native'
+
+const SignUp: React.FC = () => {
+  const dispatch = useDispatch()
+  const navigation = useNavigation()
+
+  const submitForm = async (form: object) => {
+    console.log(form)
+
+    // dispatch(signUpAction(form)).then(result => {
+    //   if (result?.data?.token) {
+    //     navigation.navigate('MainTab');
+    //     Alert.alert('Sucesso', 'Cadastro realizado com sucesso!');
+    //   }
+    // });
+  }
+
+  return (
+    <>
+      <FormSignUp submit={submitForm} />
+    </>
+  )
+}
+
+export default SignUp
