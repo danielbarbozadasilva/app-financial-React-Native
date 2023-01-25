37e72a5ba31b8a028fe162c4bc195c1da48a1beb
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-01-25 14:13:31 -0300

    feat: ensure list the deposits made to the customer's account

---
+++ src/pages/Deposit/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, FlatList } from 'react-native'
+import { decodeToken } from '../../config/auth'
+import { useDispatch, useSelector } from 'react-redux'
+import { listByIdUserDepositAction } from '../../store/transaction/transaction.action'
+import Title from '../../components/Title'
+import { styles } from './styled'
+import MovimentDeposit from '../../components/MovimentDeposit'
+import Header from '../../components/Header'
+
+const Deposit: React.FC = () => {
+  const transactions = useSelector((state: any) => state.transaction.deposit)
+
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    decodeToken().then((result) => {
+      dispatch(listByIdUserDepositAction(result.id))
+    })
+  }, [])
+
+  return (
+    <>
+      <View>
+        <Header name='Movimentações' />
+        <Title text="Depósitos" />
+
+        <FlatList
+          style={styles.list}
+          data={transactions}
+          keyExtractor={(item) => String(item.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => <MovimentDeposit data={item} />}
+        />
+      </View>
+    </>
+  )
+}
+
+export default Deposit
